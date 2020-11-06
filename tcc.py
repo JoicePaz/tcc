c1bae44e4e732f991e87d0200db353c29b3bdd9c
Joice Paz <joicepazsilva@gmail.com>
2020-11-06 18:38:18 -0300

    Update tcc.py

--- tcc.py
+++ tcc.py
@@ -2,17 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 np.random.seed(3)
-Estados = ['PA','PD']
+Estados = ['CA','PD','PA']
 
-TransicaoEstados = [['CACA','CAPA'],['CACA','CAPD']]
-MatrizTransicao = [[0.05,0.95],[0.35,0.65]]
+TransicaoEstados = [['CACA','CAPD','CAPA'], ['PDCA','PDPD','PDPA'], ['PACA','PAPD','PAPA']]
+MatrizTransicao = [[0.05,0.02,0.93],[0.18,0.80,0.02],[0.05,0.02,0.93]]
 
 listaConsumo = list()
 NumDias = 365
 PrevisaoHoje = Estados[0]
 
 for i in range(1, NumDias):
-  if PrevisaoHoje == 'PA':        
+  if PrevisaoHoje == 'CA':        
     Condicao = np.random.choice(TransicaoEstados[0], replace = True, p = MatrizTransicao[0])
 
     if Condicao == 'CAPA':
