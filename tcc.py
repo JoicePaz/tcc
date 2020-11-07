a3e25fd6dacd76ad0ea7b065fc046784ea8ab721
Joice Paz <joicepazsilva@gmail.com>
2020-11-07 16:11:12 -0300

    melhorando verificação da transição dos estados

--- tcc.py
+++ tcc.py
@@ -11,21 +11,40 @@ listaConsumo = list()
 NumDias = 365
 PrevisaoHoje = Estados[0]
 
+# 7,6 bilhões de pessoas
+# 9 quintilhões e setecentos e sessenta e cinco quatrilhões de litros de água potável 
+# 133 litros diariamente por pessoa sem considerar a carne
+# quem come carne 4297 litros diariamente de água 
+
 for i in range(1, NumDias):
   if PrevisaoHoje == 'CA':        
     Condicao = np.random.choice(TransicaoEstados[0], replace = True, p = MatrizTransicao[0])
 
-    if Condicao == 'CAPA':
+    if Condicao == 'CACA':
       pass
-    else:
+    elif Condicao == 'CAPD':
       PrevisaoHoje = 'PD'
+    elif Condicao == 'CAPA':
+      PrevisaoHoje = 'PA'
 
   elif PrevisaoHoje == 'PD':
     Condicao = np.random.choice(TransicaoEstados[1], replace = True, p = MatrizTransicao[1])
 
-    if Condicao == 'CAPD':
+    if Condicao == 'PDPD':
       pass
-    else:
+    elif Condicao == 'PDCA':
+      PrevisaoHoje = 'CA'
+    elif Condicao == 'PDPA':
+      PrevisaoHoje = 'PA'
+
+  elif PrevisaoHoje == 'PA':
+    Condicao = np.random.choice(TransicaoEstados[2], replace = True, p = MatrizTransicao[2])
+
+    if Condicao == 'PAPA':
+      pass
+    elif Condicao == 'PAPD':
+      PrevisaoHoje = 'PD'
+    elif Condicao == 'PACA':
       PrevisaoHoje = 'PA'
   
   listaConsumo.append(PrevisaoHoje)
